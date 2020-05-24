3fa147214ff06c79e4b0c41d54b13a259035c0a3
khushal <vyas.khushal1999@gmail.com>
2020-05-24 16:06:56 +0530

    adding more components

--- book-my-hotel/src/pages/Register.js
+++ book-my-hotel/src/pages/Register.js
@@ -16,14 +16,22 @@ const Register = () => {
                 <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="email" name="email-address"  id="email-address" />
             </div>
-            <div classNameName="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
-                <input className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="password" name="password"  id="password" />
+            <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="name">Select Room of your Choice</label>
+                <select className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" id="select">
+                    <option value = "Single Room"> Single Room </option>
+                    <option value = "Delux Room"> Delux Room </option>
+                    <option value = "Family Room"> Family Room </option>
+                </select>
+            </div>
+            <div className="mt3">
+                <label className="db fw6 lh-copy f6" htmlFor="number">Number of Rooms</label>
+                <input className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100" type="number" name="rooms"  id="rooms" />
             </div>
             </fieldset>
             <div className="">
             <Link to="/booked" className="btn-primary">
-            Register
+            Book Rooms
             </Link>
             </div>
         </div>
